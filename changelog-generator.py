0beef5c2e40bbc5647e2925e483359f9dd24f834
Tereza Stoyanova <stoyanova.tereza@gmail.com>
2020-01-11 21:40:23 -0500

    fix: Added the logs as arrays in the dictionaries to be able to append more

--- changelog-generator.py
+++ changelog-generator.py
@@ -12,37 +12,38 @@ def main():
     for log in logs:
         if "feature:" in log:
             if "features" in formatted_logs:
+                print(formatted_logs)
                 formatted_logs["features"].append(log)
             else:
-                formatted_logs["features"] = log
+                formatted_logs["features"] = [log]
         elif "chore:" in log:
             if "chore" in formatted_logs:
                 formatted_logs["chore"].append(log)
             else:
-                formatted_logs["chore"] = log
+                formatted_logs["chore"] = [log]
         elif "docs:" in log:
             if "docs" in formatted_logs:
                 formatted_logs["docs"].append(log)
             else:
-                formatted_logs["docs"] = log
+                formatted_logs["docs"] = [log]
         elif "fix:" in log:
             if "fix" in formatted_logs:
                 formatted_logs["fix"].append(log)
             else:
-                formatted_logs["fix"] = log
+                formatted_logs["fix"] = [log]
         elif "refactor:" in log:
             if "refactor" in formatted_logs:
                 formatted_logs["refactor"].append(log)
             else:
-                formatted_logs["refactor"] = log
+                formatted_logs["refactor"] = [log]
         elif "test:" in log:
             if "test" in formatted_logs:
                 formatted_logs["test"].append(log)
             else:
-                formatted_logs["test"] = log
+                formatted_logs["test"] = [log]
         else:
             formatted_logs["uncategorized"].append(log)
-        
+
     print("The logs are:\n", formatted_logs)
 
 
